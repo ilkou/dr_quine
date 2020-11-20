37b9d9c86c912321df42a3d9f9d5ad752347f041
ilkou <ouklichoussama@gmail.com>
2020-11-20 21:35:29 +0100

    JS DONE++

--- JS/Sully.js
+++ JS/Sully.js
@@ -7,7 +7,10 @@ i = fs.existsSync('./Sully_5.js') ? i : 6;
 
 (function Sully() {
 	if (i > 0) {
-		fs.writeFile("Sully_" + (i - 1).toString() + ".js", "const fs = require('fs');\nconst { exec } = require('child_process');\n\nlet i = " + (i - 1).toString() + ";\n\ni = fs.existsSync('./Sully_5.js') ? i : 6;\n\n(" + Sully.toString() + ")()\n", (err) => {});
-		exec("node Sully_" + (i - 1).toString() + ".js", (err, stdout, stderr) => {});
+		let data = "const fs = require('fs');\nconst { exec } = require('child_process');\n\nlet i = " + (i - 1).toString() + ";\n\ni = fs.existsSync('./Sully_5.js') ? i : 6;\n\n(" + Sully.toString() + ")()\n";
+		fs.writeFile("Sully_" + (i - 1).toString() + ".js", data, (err) => {});
+		fs.writeFile("Sully_" + (i - 1).toString(), "#!/usr/bin/env node\n" + data, (err) => {});
+		exec("chmod +x Sully_" + (i - 1).toString(), (err, stdout, stderr) => {});
+		exec("./Sully_" + (i - 1).toString(), (err, stdout, stderr) => {});
 	}
 })()
